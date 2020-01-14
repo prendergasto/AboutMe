608e359fd57a0d45389a3817468e7b69b9b89f43
Owen Prendergast <prendergasto@gmail.com>
2020-01-13 17:54:56 -0800

    about me version 2

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-export default function quiz() => {
-    const prompt = 
+import isYes from './is-yes.js'
 
+const quizButton = document.getElementById('quizButton');
 
+quizButton.addEventListener('click', () => {
+    console.log = ('button works!');
 
+    const name = prompt('What is your name?');
+    const confirmation = confirm (`${name}, are you sure you want to take the quiz?`);
+    if(confirmation === false) return;
+    const answer1 = prompt('Am I from Portland?');
+    const answer2 = prompt('Did I go to school in Boulder?');
+    const answer3 = prompt('Do I like cats?');
+    const answer4 = prompt('Do I like the way ice cream makes me feel?');
+    let count = 0;
+    if(isYes(answer1)) count ++;
+    if(isYes(answer2)) count ++;
+    if(!isYes(answer3)) count ++;
+    if(!isYes(answer4)) count ++;
+    total.textContent=(`${name}, you scored a ${count} on my quiz`)
+});
 
-
-
-
-
-
-}
---
+++ is-yes.js
@@ -0,0 +1,6 @@
+export default isYes;
+
+function isYes(str) {
+    if (str.toLowerCase() === 'yes' || str.toLowerCase() === 'y') return true;
+    else return false;
+}
