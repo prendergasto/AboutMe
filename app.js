57e44b671915b4ca6cead18c411b50eb00b80bd9
Owen Prendergast <prendergasto@gmail.com>
2020-01-15 11:27:12 -0800

    guessing game is finally working

--- app.js
+++ app.js
@@ -1,41 +1,44 @@
-import isYes from './is-yes.js'
+import compareNumbers from './compareNumbers.js';
 
-//retrieving the quiz button from the html document
-const quizButton = document.getElementById('quizButton');
-const total = document.getElementById('total');
+const userInput = document.getElementById('input');
+const guessButton = document.getElementById('guess-button');
+const triesRemaining = document.getElementById('tries-remaining');
+const guessContainer = document.getElementById('guess-container');
+const guessResult = document.getElementById('guess-result');
+const answerDisplay = document.getElementById('answer-display');
 
-// adding an event listener to the "click" on the website
-quizButton.addEventListener('click', () => {
-    console.log = ('button works!');
+let magicNumber = 13;
 
-// the first prompt after enganging the "take quiz" button on the website
-    const name = prompt('What is your name?');
-// confirming whether or not the user wants to take the quiz
-    const confirmation = confirm (`${name}, are you sure you want to take the quiz?`);
-// if the user clicks "cancel" it will register as "false" and return the user to the mainpage
-    if (confirmation === false) return;
-// prompt #2 / question 1
-    const answer1 = prompt('Am I from Portland?');
-// makes the "cancel" button functional
-    if (answer1 === null) return;
-    const answer2 = prompt('Did I go to school in Boulder?');
-    if (answer2 === null) return;
-    const answer3 = prompt('Do I like cats?');
-    if (answer3 === null) return;
-    const answer4 = prompt('Do I like the way ice cream makes me feel?');
-    if (answer4 === null) return;
-// "let" makes the count dynamic and able to change according to how well or how badly the user does on the quiz
-    let count = 0;
-// depending on the user's answer this will add a point, or not, if the answer is right or wrong
-    if (isYes(answer1)) count ++;
-    if (isYes(answer2)) count ++;
-    if (!isYes(answer3)) count ++;
-    if (!isYes(answer4)) count ++;
-// this takes the total # correct and the name of the user and prints a statement on the results
-    total.textContent = `${name}, you scored a ${count} on my quiz`;
-// if the person gets a 3 or a 4 they get a "good job!" message
-    if (count >= 3) total.textContent = `${name}, you scored a ${count} on my quiz. Good job!`;
-    // tried doing an if greater than statement, but that wasn't working
+let tries = 4;
+
+
+guessButton.addEventListener('click', () => {
+    tries --;
+    triesRemaining.textContent = tries;
+    if (compareNumbers(Number(userInput.value), magicNumber) === -1) {
+        guessContainer.classList.remove('hidden');
+        guessResult.textContent = 'too low!';
+    }
+    if (compareNumbers(Number(userInput.value), magicNumber) === 1) {
+        guessContainer.classList.remove('hidden');
+        guessResult.textContent = 'too high!';
+    }
+    // if (compareNumbers(Number(userInput.value), magicNumber) === 0.1) {
+    //     guessContainer.classList.remove('hidden');
+    //     guessResult.textContent = 'You\'re getting warmer!';
+    // }
+    if (compareNumbers(Number(userInput.value), magicNumber) === 0) {
+        guessContainer.classList.remove('hidden');
+        guessResult.textContent = 'correct!';
+        answerDisplay.classList.remove('hidden');
+        guessButton.disabled = true;
+    }
+    if (tries == 0 && compareNumbers(Number(userInput.value), magicNumber) !== 0) {
+        guessButton.disabled = true;
+        answerDisplay.classList.remove('hidden');
+        answerDisplay.textContent = 'You lose!';
+
+    }
+
+
 });
-
-
---
+++ compareNumbers.js
@@ -0,0 +1,13 @@
+export default function compareNumbers(userGuess, magicNumber) {
+    if (userGuess === magicNumber) {
+        return 0;
+    } else if (userGuess > magicNumber) {
+        return 1;
+    } else if (userGuess < magicNumber) {
+        return -1;
+    } 
+    // else if (11 <= userGuess >= 15) {
+    //     return 0.1;
+    // }
+
+}
